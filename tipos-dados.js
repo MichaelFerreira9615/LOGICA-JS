864fd40f6edff3cdd13080087a40f8e3c86f6495
unknown <michaelholanda6324@digitalcollege.internal>
2025-11-25 21:44:14 -0300

    primeiro commit

---
+++ tipos-dados.js
@@ -0,0 +1,101 @@
+// tipo texto - string
+// "Isso é um texto";
+// "Rodrigo";
+// "O tenis nike é composto...";
+// "123";
+// 'Isso tambem é um texto mesmo com aspas simples';
+// `Usar a crase o JS entende como string`;
+
+// tipo number
+// 100 //inteiros - int
+// 25.35//decimais - float
+
+// tipo booleano - boolean - true / false
+// undefined
+
+// tipo nulo - null
+// null 
+//O que é Variavel?
+
+ var meuSobrenome = "Silva"; //não usamos mais
+ let caraDaCamisaVinho = "Rodrigo";//usamos
+ const altura = 1.9;//usamos
+//padrão de escrita chamada CamelCase
+
+ console.log(caraDaCamisaVinho);
+
+ caraDaCamisaVinho ="Anderson";
+
+ console.log(caraDaCamisaVinho)
+
+ let temCarro = true;
+ let isOpen = false
+
+ let profissão = undefined
+
+ const meuNulo = null
+
+ console.log(profissão)
+ console.log(meuNulo)
+
+// Crie 3 variaveis da sua escolha sa sua escolha com let e 3 variaveis da sua escolha com const. Não repetir miha variaveis com let.. e apresente todos os valores no console.
+
+const peso = 120
+const nome = "Michael"
+const treinando = false
+
+console.log(peso);
+console.log(nome);
+console.log(treinando);
+
+let póBranco = "Farinha";
+let cargo = "aux.administrativo";
+let temperaturaMedia = 30;
+
+póBranco = "Trigo"
+cargo = "telecom"
+temperaturaMedia = 32
+console.log(póBranco);
+console.log(cargo);
+console.log(temperaturaMedia);
+
+// operações matematicas
+
+let numero1 = 20;
+let numero2 = 10;
+
+let soma = numero1 + numero2;
+
+console.log(soma);
+
+soma = 50
+
+numero2 = 5;
+
+console.log(soma);
+
+let resulSubtracao = soma - numero2
+
+console.log(resulSubtracao)
+
+let result3 = 30 * 3;
+
+console.log(result3)
+
+let result4 = 50 / 10;
+
+console.log(result4)
+
+// mod - resto da divisao
+
+let result5 = 10 % 3;
+
+console.log(result5)
+
+// verificanso no console os tipos de dados - typeof
+
+console.log(typeof caraDaCamisaVinho)
+console.log(typeof altura)
+console.log(typeof nome)
+console.log(typeof undefined)
+console.log(typeof meuNulo)
