e382accc7ab78b81f08c900db9de7a7e920ebaaf
unknown <michaelholanda6324@digitalcollege.internal>
2025-12-02 19:58:45 -0300

    segundo commit

---
+++ 3-intro-arrays.js
@@ -0,0 +1,14 @@
+//assuntos que ja aprendemos:
+//tipos de dados primitivo - string, number, booleano, undefined, null
+// varialções da string - aspas duplas, aspas simples, crase (template string)
+//crição de variaveis - var, let, const
+//diferença do let e const
+//reatribuicao de valores na variavel
+//operadores matematicos
+//operadores de comparpação
+//operadores logico (e, ou, inverso)
+//propriedade typeof
+
+//assunto: introdução a arrays (listas)
+
+const frutas = ["laranja, "];
