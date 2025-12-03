fc674c4a1addb35bd443a1847919a33cde09d473
unknown <michaelholanda6324@digitalcollege.internal>
2025-12-02 21:52:55 -0300

    terceiro commmit

--- 3-intro-arrays.js
+++ 3-intro-arrays.js
@@ -11,4 +11,40 @@
 
 //assunto: introdução a arrays (listas)
 
-const frutas = ["laranja, "];
+const frutas = ["laranja", "morango", "manga", "uva"];
+console.log(frutas);
+
+console.log(frutas[1]);
+console.log(frutas[3]);
+console.log(frutas[0]);
+console.log(frutas[4-2]);
+
+console.log(frutas [0] === "banana");
+
+const objeto = "dindin" || "geladinho";
+
+const resultado = objeto === "dindin";
+
+console.log(resultado)
+
+//substituir o valor no array
+
+frutas[2] = "banana";
+
+console.log(frutas)
+
+frutas[4] = "manga"
+
+console.log(frutas)
+
+frutas[6] = "fruta do conde"
+console.log(frutas);
+
+console.log(frutas[5]);
+
+//quantidade de intems de um arreio 
+
+console.log(frutas.length)
+
+console.log(frutas[frutas.length -1])
+
---
+++ 4-intro-objetos.js
@@ -0,0 +1,55 @@
+// instrodução objetos
+const aluno = {
+    ano:3,
+    matricula: 243445325,
+    turma: "B",
+    ano: 3,
+}
+
+const pessoa = {
+    nome: "Marcos",
+    idade: 25,
+    temCarro: true,
+    casado: false,
+    coresFavoritas: ["verde", "preto", "azul"],
+    carro: {
+        modelo: "corolla",
+        ano: 2022,
+        itens: ["direcao eletrica", "cvt", "banco de couro"],
+    },
+};
+
+console.log(aluno);
+console.log(aluno.matricula);
+console.log(aluno.nome);
+console.log(aluno.nome === pessoa.nome);
+console.log(pessoa.coresFavoritas)
+console.log(pessoa.coresFavoritas[2]);
+console.log(pessoa.carro.modelo)
+console.log(pessoa.carro.itens[1]);
+
+// crie uma variavel chamado frase, e coloque uma string "Eu tenho um carro com cambio [cvt]" - use template string
+
+const frase = `Eu tenho um ${pessoa.carro.modelo} com cambio ${pessoa.carro.itens[1]}`;
+
+console.log(frase)
+
+// substituindo valores em um objeto
+
+pessoa.carro.ano = 2025;
+
+console.log(pessoa);
+
+// adicionando ar no itens do carro 
+pessoa.carro.itens[3] = "ar"
+console.log(pessoa)
+
+// deletando uma propriedade 
+
+delete pessoa.casado
+
+console.log(pessoa);
+
+pessoa.profissao = "programador"
+
+console.log(pessoa);
